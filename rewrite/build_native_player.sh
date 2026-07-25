43b44714f1ef808223fcdc7529550448e5430339
xor2003 <>
2026-07-25 15:12:51 +0200

    Validate self-contained Windows release package

--- rewrite/build_native_player.sh
+++ rewrite/build_native_player.sh
@@ -3,6 +3,11 @@ set -eu
 cd "$(dirname "$0")/.."
 mkdir -p rewrite/.build
 
+native_link_flags=
+case "$(uname -s)" in
+  MINGW*|MSYS*) native_link_flags="-static-libgcc -static-libstdc++" ;;
+esac
+
 cc -std=c99 -O0 -Wall -Wextra -Irewrite -c rewrite/iplay_rewrite.c -o rewrite/.build/iplay_rewrite_for_modplug_audio.o
 c++ -std=c++17 -O0 -Wall -Wextra $(pkg-config --cflags libmodplug sdl2 SDL2_image notcurses) -Irewrite \
   rewrite/modplug_renderer.cpp \
@@ -13,6 +18,7 @@ c++ -std=c++17 -O0 -Wall -Wextra $(pkg-config --cflags libmodplug sdl2 SDL2_imag
   rewrite/modplug_audio_probe.cpp \
   rewrite/.build/iplay_rewrite_for_modplug_audio.o \
   $(pkg-config --libs libmodplug sdl2 SDL2_image notcurses) \
+  $native_link_flags \
   -o rewrite/.build/iplay_modern_host
 install_player_atomically() {
   src=$1
